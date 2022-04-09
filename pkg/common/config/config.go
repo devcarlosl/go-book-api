298cf239d70ad318852193bffe9f612173b03853
Carlos Lopes <dev.carloslopes@gmail.com>
2022-04-09 12:18:54 -0300

    fix config struct

--- pkg/common/config/config.go
+++ pkg/common/config/config.go
@@ -3,12 +3,8 @@ package config
 import "github.com/spf13/viper"
 
 type Config struct {
-	Port   string `mapstructure:"PORT"`
-	DBHost string `mapstructure:"DB_HOST"`
-	DBUser string `mapstructure:"DB_USER"`
-	DBPass string `mapstructure:"DB_PASS"`
-	DBName string `mapstructure:"DB_NAME"`
-	DBPort string `mapstructure:"DB_PORT"`
+	Port  string `mapstructure:"PORT"`
+	DBUrl string `mapstructure:"DB_URL"`
 }
 
 func LoadConfig() (c Config, err error) {
